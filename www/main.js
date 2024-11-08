234b49bc499751eb055ea1611703eaee4a730a20
yogidc <158734666+yogidc@users.noreply.github.com>
2024-11-08 21:29:39 +0530

    hood design done

---
+++ www/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.text').textilate({
+        loop:true,
+        sync:true,
+        in:{
+            effect:"bouncein",
+        },
+        out:{
+            effect:"bounceout",
+        },
+    });
+});
